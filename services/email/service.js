d40a1970eab65d1141aa588970f64641e963b453
Alexander Krupka <bamaboy483@gmail.com>
2022-02-18 20:40:30 +0200

    Update service.js

--- services/email/service.js
+++ services/email/service.js
@@ -8,7 +8,7 @@ class EmailService {
         this.link = " https://8a8d-185-159-163-198.ngrok.io";
         break;
       case "production":
-        this.link = `http://localhost:3000`;
+        this.link = `https://phonebooknodejs.herokuapp.com`;
         break;
       default:
         this.link = "http://127.0.0.1:3000";
