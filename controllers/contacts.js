64773280945d77daa5ac1f421ef583349263cac9
Alexander Krupka <bamaboy483@gmail.com>
2022-05-13 19:17:59 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,8 +4,12 @@ const { HttpCode } = require("../config/constant");
 
 const getContacts = async (req, res) => {
   const userId = req.user._id;
-  const data = await Contacts.listContacts(userId, req.query);
-  res.json({ status: "success", code: HttpCode.OK, data: { ...data } });
+  const { pageInfo, contacts } = await Contacts.listContacts(userId, req.query);
+  res.json({
+    status: "success",
+    code: HttpCode.OK,
+    data: { contacts, pageInfo },
+  });
 };
 
 const getContact = async (req, res, next) => {
@@ -22,12 +26,15 @@ const getContact = async (req, res, next) => {
 
 const saveContact = async (req, res, next) => {
   const userId = req.user._id;
-  const contact = await Contacts.addContact({ ...req.body, owner: userId });
+  const { contact, pageInfo } = await Contacts.addContact({
+    ...req.body,
+    owner: userId,
+  });
   if (contact) {
     return res.json({
       status: "success",
       code: HttpCode.CREATED,
-      data: { contact },
+      data: { contact, pageInfo },
     });
   }
 
@@ -37,13 +44,16 @@ const saveContact = async (req, res, next) => {
 const removeContact = async (req, res, next) => {
   const userId = req.user._id;
 
-  const contact = await Contacts.removeContact(req.params.contactId, userId);
+  const { contact, pageInfo } = await Contacts.removeContact(
+    req.params.contactId,
+    userId
+  );
   if (contact) {
     return res.status(HttpCode.OK).json({
       status: "success",
       code: HttpCode.OK,
       message: "Deleted",
-      data: { contact },
+      data: { contact, pageInfo },
     });
   }
 
