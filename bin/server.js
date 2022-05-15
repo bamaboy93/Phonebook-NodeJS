da7454f604e8ebefd6fba149d799daba64414bc7
Alexander Krupka <bamaboy483@gmail.com>
2022-05-15 19:04:08 +0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -4,11 +4,15 @@ const app = require("../app");
 require("dotenv").config();
 
 const mkdirp = require("mkdirp");
+const UPLOAD_DIR = process.env.UPLOAD_DIR;
+const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS;
 
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
   app.listen(PORT, async () => {
+    mkdirp(UPLOAD_DIR);
+    mkdirp(AVATAR_OF_USERS);
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
