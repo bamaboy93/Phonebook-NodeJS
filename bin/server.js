7e57e5cdc67897c8fd6b13dad2f4c92b1a9e319b
Alexander Krupka <bamaboy483@gmail.com>
2022-05-15 17:51:25 +0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,19 +1,23 @@
-const app = require("../app");
 const db = require("../config/db");
-require("dotenv").config();
+const app = require("../app");
+
+const result = require("dotenv").config();
+
 const UPLOAD_DIR = process.env.UPLOAD_DIR;
 const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS;
 const mkdirp = require("mkdirp");
+if (result.error) {
+  throw result.error;
+}
 
-require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
-  app.listen(PORT, async () => {
-    await mkdirp(UPLOAD_DIR);
-    await mkdirp(AVATAR_OF_USERS);
-    console.log(`Server is running. Use our API on port: ${PORT}`);
+  app.listen(PORT, () => {
+    mkdirp(UPLOAD_DIR);
+    mkdirp(AVATAR_OF_USERS);
+    console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
-  console.log(`Server failed to run. Error: ${err.message}`);
+  console.log(`Server not run. Error: ${err.message}`);
 });
