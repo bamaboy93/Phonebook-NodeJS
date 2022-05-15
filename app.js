40faf1ad80086f0525f45893cc9eb29755ee94e4
Alexander Krupka <bamaboy483@gmail.com>
2022-05-15 18:18:03 +0300

    ++

--- app.js
+++ app.js
@@ -23,14 +23,13 @@ app.use(helmet());
 
 app.use(logger(formatsLogger));
 app.use(
-  cors()
-  // cors({
-  //   origin: "*",
-  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   credentials: true,
-  // })
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  })
 );
 app.use(express.json({ limit: 10000 }));
 app.use(boolParser());
